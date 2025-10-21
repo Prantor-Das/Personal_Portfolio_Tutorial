39d23db52d02a1b40e5dec48c901e4e39627cb31
Prantor-Das <prantor.college@gmail.com>
2025-10-21 13:57:26 +0530

    add Home page layout and components; implement About, Hero, Navbar, Star background, and Theme toggle

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
+import { ThemeToggle } from '../components/ThemeToggle'
+import { StarBackground } from '../components/StarBackground'
+import { Navbar } from '../components/Navbar'
+import { HeroSection } from '../components/HeroSection'
+import { AboutSection } from '../components/AboutSection'
 
 function Home() {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
         {/* Theme Toggle Button */}
+        <ThemeToggle />
 
         {/* Background Effects */}
+        <StarBackground />
 
         {/* Navbar */}
+        <Navbar />
 
         {/* Main Content */}
+        <main>
+          <HeroSection />
+          <AboutSection />
+        </main>
 
         {/* Footer */}
     </div>
