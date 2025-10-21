e707b6aa8b85c5046728506e6594514a9bfc9a8e
Prantor-Das <prantor.college@gmail.com>
2025-10-21 10:52:23 +0530

    initialize portfolio project with React, Vite, and Tailwind CSS

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className="min-h-screen">
+        {/* Theme Toggle Button */}
+
+        {/* Background Effects */}
+
+        {/* Navbar */}
+
+        {/* Main Content */}
+
+        {/* Footer */}
+    </div>
+  )
+}
+
+export default Home
